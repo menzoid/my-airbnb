9515f2a18275d901a2d213a3cbcf6c19be8aa809
Ryan Brouwer <mennofromheavan@gmail.com>
2022-10-25 12:48:37 +0200

    final code from livecode

---
+++ test/system/flats_test.rb
@@ -0,0 +1,21 @@
+require "application_system_test_case"
+
+class FlatsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit flats_url
+  #
+  #   assert_selector "h1", text: "Flats"
+  # end
+  test "visiting flats page" do
+    visit "/"
+
+    assert_selector "h1", text: "Flats"
+    assert_selector "h2", text: "Trendy Apt in Buttes Montmartre"
+  end
+
+  test "visiting the first flat page" do
+    visit "/flats/145"
+
+    assert_selector "h1", text: "Charm at the Steps of the Sacre Coeur/Montmartre"
+  end
+end
